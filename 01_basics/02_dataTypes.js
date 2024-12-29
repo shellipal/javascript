b9bcfb4afea196d7f194ccca4819f75f636320a5
shellipal <palshelli5@gmail.com>
2024-12-29 21:51:58 +0530

    learn dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+// alert( 3 + 3 ) // we are using nodejs , not browser
+
+let name = "shelli";
+console.log(name);
+
+let age = 18;
+let isLoggedIn = true;
+
+//number => 2 to power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// objects
+
+console.log(typeof undefined); // underfined
+console.log(typeof null); // object
