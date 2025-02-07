883457551c73954c699a3acbdec205659267b92c
shellipal <palshelli5@gmail.com>
2025-02-07 23:52:10 +0530

    learn promises

---
+++ 09_advanceOne/Promises.js
@@ -0,0 +1,55 @@
+const PromiseOne = new Promise(function (resolve, reject) {
+  // Do an asyns task...........
+  // DB calls , cryptography , network
+  setTimeout(function () {
+    console.log("Async task is complete");
+    resolve();
+  }, 1000);
+});
+
+PromiseOne.then(function () {
+  console.log("Promise consumed");
+});
+
+new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    console.log("Async task 2 complete");
+    resolve();
+  }, 1000);
+}).then(function () {
+  console.log("Async 2 resolved");
+});
+
+const promiseThree = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    resolve({ username: "Shelli", email: "shellipal5@gmail.com" });
+  }, 1000);
+});
+
+promiseThree.then(function (user) {
+  console.log(user);
+});
+
+const promiseFour = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    let error = false;
+    if (!error) {
+      resolve({ username: "Shelli", password: "1234" });
+    } else {
+      reject("ERROR : Something went wrong");
+    }
+  }, 1000);
+});
+
+promiseFour
+  .then((user) => {
+    console.log(user);
+    return user.username;
+  })
+  .then((username) => {
+    console.log(username);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .finally(() => console.log("The promise is either resolved or rejected"));
