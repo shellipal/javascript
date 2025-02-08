93c383913d035cc2366eee293fbdcdff87f48273
shellipal <palshelli5@gmail.com>
2025-02-08 20:39:34 +0530

    async,await

--- 09_advanceOne/Promises.js
+++ 09_advanceOne/Promises.js
@@ -61,3 +61,47 @@ promiseFour
     console.log(error);
   })
   .finally(() => console.log("The promise is either resolved or rejected"));
+
+const promiseFive = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    let error = true;
+    if (!error) {
+      resolve({ username: "javascript", password: "123" });
+    } else {
+      reject("ERROR : JS went wrong");
+    }
+  }, 1000);
+});
+
+async function consumepromiseFive() {
+  try {
+    const response = await promiseFive;
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+}
+consumepromiseFive();
+
+// async function getAllUser() {
+//   try {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//   const data = await response.json();
+//   console.log(data);
+//   } catch (error) {
+//     console.log("error");
+
+//   }
+// }
+// getAllUser();
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
