ee2738f067cdfaeec5ccd576c059d23c0c1a19d7
shellipal <palshelli5@gmail.com>
2025-02-18 22:03:53 +0530

    properties

---
+++ 10_Classes&oopInJS/11_gettter&setter.js
@@ -0,0 +1,23 @@
+class User {
+  constructor(email, password) {
+    this.email = email;
+    this.password = password;
+  }
+
+  get email() {
+    return this._email.toUpperCase();
+  }
+  set email(value) {
+    this._email = value;
+  }
+
+  get password() {
+    return this._password.toUpperCase();
+  }
+  set password(value) {
+    this._password = value;
+  }
+}
+
+const Shelli = new User("shelli@gmail.com", "abc");
+console.log(Shelli.email);
